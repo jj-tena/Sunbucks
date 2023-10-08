96951998834665c910f1493bcec32f0bb50d59f5
jj-tena <josejustotena0@gmail.com>
2023-10-08 17:32:46 +0200

    Add check inventory method in orders service

---
+++ microservices/orders-service/src/main/java/com/jjtena/ordersservice/model/dtos/BaseResponse.java
@@ -0,0 +1,8 @@
+package com.jjtena.ordersservice.model.dtos;
+
+public record BaseResponse(String[] errorMessages) {
+
+    public boolean hasErrors() {
+        return errorMessages != null && errorMessages.length > 0;
+    }
+}
--- microservices/orders-service/src/main/java/com/jjtena/ordersservice/service/OrderService.java
+++ microservices/orders-service/src/main/java/com/jjtena/ordersservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.jjtena.ordersservice.service;
 
+import com.jjtena.ordersservice.model.dtos.BaseResponse;
 import com.jjtena.ordersservice.model.dtos.OrderItemRequest;
 import com.jjtena.ordersservice.model.dtos.OrderRequest;
 import com.jjtena.ordersservice.model.entities.Order;
@@ -7,7 +8,9 @@ import com.jjtena.ordersservice.model.entities.OrderItem;
 import com.jjtena.ordersservice.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,14 +18,30 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
 
     public void addOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setOrderItems(orderRequest.getOrderItemRequests().stream()
-                .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
-                .toList());
-        this.orderRepository.save(order);
+        BaseResponse baseResponse = checkInventory(orderRequest.getOrderItemRequests());
+        if (baseResponse != null && !baseResponse.hasErrors()) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setOrderItems(orderRequest.getOrderItemRequests().stream()
+                    .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
+                    .toList());
+            this.orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Some of the products are not in stock");
+        }
+    }
+
+    private BaseResponse checkInventory(List<OrderItemRequest> orderItemRequests) {
+        return this.webClientBuilder.build()
+                .post()
+                .uri("http://localhost:8083/api/inventory/in-stock")
+                .bodyValue(orderItemRequests)
+                .retrieve()
+                .bodyToMono(BaseResponse.class)
+                .block();
     }
 
     private OrderItem mapOrderItemRequestToOrderItem(OrderItemRequest orderItemRequest, Order order) {
@@ -34,6 +53,5 @@ public class OrderService {
                 .order(order)
                 .build();
     }
-
 
 }
