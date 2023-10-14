c7b07eaa44748bd73527d322339cb2a948604a44
jj-tena <josejustotena0@gmail.com>
2023-10-14 18:00:02 +0200

    Create mobile frontend project scaffolding

---
+++ frontend/frontend-mobile/components/database/products.ts
@@ -0,0 +1,74 @@
+export const Products = [
+    {
+        id: 1,
+        category: 'coffee',
+        name: 'Cafe con leche',
+        price: 5,
+        description: 'Exquisito cafe con leche',
+        image: require('./images/coffees/coffee1.jpeg')
+    },
+    {
+        id: 2,
+        category: 'coffee',
+        name: 'Cafe solo',
+        price: 6,
+        description: 'Exquisito cafe solo',
+        image: require('./images/coffees/coffee2.jpg')
+    },
+    {
+        id: 3,
+        category: 'coffee',
+        name: 'Cafe bombón',
+        price: 8,
+        description: 'Exquisito cafe bombón',
+        image: require('./images/coffees/coffee3.jpg')
+    },
+    {
+        id: 4,
+        category: 'cake',
+        name: 'Tarta de chocolate',
+        price: 5,
+        description: 'Exquisita tarta de chocolate',
+        image: require('./images/cakes/cake1.jpg')
+    },
+    {
+        id: 5,
+        category: 'cake',
+        name: 'Tarta red velvet',
+        price: 5,
+        description: 'Exquisita tarta red velvet',
+        image: require('./images/cakes/cake2.jpg')
+    },
+    {
+        id: 6,
+        category: 'cake',
+        name: 'Tarta de queso',
+        price: 6,
+        description: 'Exquisita tarta de queso',
+        image: require('./images/cakes/cake3.jpg')
+    },
+    {
+        id: 7,
+        category: 'milkshake',
+        name: 'Batido de fresa',
+        price: 7,
+        description: 'Exquisito batido de fresa',
+        image: require('./images/milkshakes/milkshake1.jpg')
+    },
+    {
+        id: 8,
+        category: 'milkshake',
+        name: 'Batido de chocolate',
+        price: 7,
+        description: 'Exquisito batido de chocolate',
+        image: require('./images/milkshakes/milkshake2.jpeg')
+    },
+    {
+        id: 9,
+        category: 'milkshake',
+        name: 'Batido de vainilla',
+        price: 7,
+        description: 'Exquisito batido de vainilla',
+        image: require('./images/milkshakes/milkshake3.jpg')
+    }
+]
