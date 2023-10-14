ebe3e061260199298cf68424ac4fed88fa13cc7b
jj-tena <josejustotena0@gmail.com>
2023-10-14 18:30:08 +0200

    Set routes

--- frontend/frontend-mobile/App.js
+++ frontend/frontend-mobile/App.js
@@ -1,9 +1,25 @@
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/screens/Home';
+import Product from './components/screens/Product';
+import Cart from './components/screens/Cart';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='Product' component={Product}/>
+        <Stack.Screen name='Cart' component={Cart}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
